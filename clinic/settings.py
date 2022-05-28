e356bf4865a1892b861034488c7f0ef59cfd8749
isaaccleopas <94404627+isaaccleopas@users.noreply.github.com>
2022-05-28 10:34:26 +0100

    Django on Heroku library added

--- clinic/settings.py
+++ clinic/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)xo4e0xne$sg^0^$ua*x)+x1$n4@icpnkdj!mc(l*w5^(f%)v-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
