d8a71dad64e79a480143f8e8d4dd4d8d39945321
isaaccleopas <94404627+isaaccleopas@users.noreply.github.com>
2022-05-30 15:14:32 +0100

    password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 from appointment.views import *
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 
@@ -11,5 +12,8 @@ urlpatterns = [
     path('therapist/profile/update/', EditTherapistProfileView.as_view(), name='therapist-profile-update'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    # path('', Password)
+    path('patient-password/', auth_views.PasswordChangeView.as_view(
+            template_name='commons/patient-password.html',
+            success_url = 'patient-password'), name='patient-password'),
+    # path('', TherapistPassword.as_view(), name='therapist-password'),
 ]
